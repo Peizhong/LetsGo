cbdc76457c82da1b0da5d02635c163efd53e0082
Peizhong Wang <w.xy.z@live.com>
2019-02-18 23:36:04 +0800

    go

---
+++ hello.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	// 定义变量
+	var b bool
+	// 定义并赋值
+	var n = 1
+	// 简短声明
+	_, v1, v2, v3 := 0, 1, 3, 5
+
+	// 常量
+	const value0 = 1234
+	value1 := 1
+	_, value2 := 2, value1
+	fmt.Println("!oG ,olleH")
+}
