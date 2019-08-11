c6f91429e396d57d06b42a64b54a01ab870ae070
peizhong <w.xy.z@live.com>
2019-08-12 00:33:55 +0800

    希尔

--- playground/cpp/src/sort.cpp
+++ playground/cpp/src/sort.cpp
@@ -24,33 +24,39 @@ namespace ADT::Sort{
             }
             // 最后的j，位置没有变，或往前移动了
             a[j] = tmp;
-        }
+        } 
     }
     
+    // 希尔排序：递减增量排序，插入排序的改进版本，单非稳定
+    // 插入排序对几乎排序好的数据，效率高
+    // 希尔排序的思想：将待排列的数据分割成子序列进行插入排序，待基本有序后，进行插入排序
     void ShellSort(int a[],int size)
     {
+        // 12个 3,1,21,4,8,16,7,14,6,13,41,11
+        // 增量为6,3,1
         int i,j,increment;
         int tmp;
         // if size=6; // 3,1
         for (increment = size/2;increment>0;increment/=2)
         {
-            // 3,4,5,6
-            // 1,2,3,4,5,6
             for (i=increment;i<size;i++)
             {
                 tmp = a[i];
-                // 从后面到前面
+                // 从后面到前面，j,j-increment,j-increment*2,j-increment*3
                 for (j=i;j>=increment;j-=increment)
                 {
+                    // 前面都排好序了，只要当前值比前面的小，一直往前进
                     if (tmp<a[j-increment])
                     {
                         a[j] = a[j-increment];
                     }
+                    // 顺序正常了就可以结束了
                     else
                     {
                         break;
                     }
                 }
+                // 当前数据的摆放位置
                 a[j]=tmp;
             }
         }
